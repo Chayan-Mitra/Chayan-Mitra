edc701d33dbb416113c6d2ccf907b5de7650ac9d
Chayan Mitra <74533220+Chayan-Mitra@users.noreply.github.com>
2022-05-11 18:24:20 +0530

    Add files via upload

---
+++ ticket.java
@@ -0,0 +1,19 @@
+package com.bitesbytes;
+
+public class ticket {
+    public static String from_destination;
+    public static String to_destination;
+    public static int validity_time;
+    public static int validity_date;
+    public static String ticket_type;
+    public static String candidate_name;
+    public static int seat_number;
+    public static String company_name;
+    public static int ticket_serial_number;
+    public static boolean food_included = false;
+    public static int length;
+    public static int width;
+    public static int price;
+    public static int candidate_age;
+    public static boolean paid;
+}
